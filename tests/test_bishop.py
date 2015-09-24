225a007f30784b98a0558bcf730cd3c3273f5384
rmathure <rmathure@indiana.edu>
2015-09-24 11:50:21 -0500

    Adding test structure for rook and bishop

---
+++ tests/test_bishop.py
@@ -0,0 +1,27 @@
+__author__ = 'rohanmathure'
+
+import unittest
+
+
+class TestBishopBase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_base_moves(self):
+        pass
+
+
+class TestBishopBlocked(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_blocked_moves(self):
+        pass
+
+
+class TestBishopAttackMoves(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_attack_moves(self):
+        pass
---
+++ tests/test_rook.py
@@ -0,0 +1,26 @@
+
+import unittest
+
+
+class TestRookBase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_base_moves(self):
+        pass
+
+
+class TestRookBlocked(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_blocked_moves(self):
+        pass
+
+
+class TestRookAttackMoves(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_attack_moves(self):
+        pass
